bf97ea69fc02016e4c4b200a841f9c7f0677401c
stefansor <ssorobay00@stuy.edu>
2019-02-11 22:30:04 -0500

    added test cases and comments provided by Mr. K

--- QueenBoard.java
+++ QueenBoard.java
@@ -59,6 +59,21 @@ public class QueenBoard{
     }
   }
 
+
+
+
+  /**
+  *@return The output string formatted as follows:
+  *All numbers that represent queens are replaced with 'Q'
+  *all others are displayed as underscores '_'
+  *There are spaces between each symbol:
+  *"""_ _ Q _
+  *Q _ _ _
+  *_ _ _ Q
+  *_ Q _ _"""
+  *(pythonic string notation for clarity,
+  *excludes the character up to the *)
+  */
   public String toString(){
     String str = "";
     for(int i = 0; i < board.length; i++){
@@ -87,7 +102,19 @@ public class QueenBoard{
   }
 
 
+  /**
+    *@return false when the board is not solveable and leaves the board filled with zeros;
+    *        true when the board is solveable, and leaves the board in a solved state
+    *@throws IllegalStateException when the board starts with any non-zero value
+    */
   public boolean solve(){
+    for(int i = 0; i < board.length; i++){
+      for(int j = 0; j < board.length; j++){
+        if(board[i][j] != 0){
+          throw new IllegalStateException("Board must be cleared before finding a solution");
+        }
+      }
+    }
     return help(0);
   }
 
@@ -107,7 +134,24 @@ public class QueenBoard{
     return false;
   }
 
+
+
+
+
+
+
+  /**
+  *@return the number of solutions found, and leaves the board filled with only 0's
+  *@throws IllegalStateException when the board starts with any non-zero value
+  */
   public int countSolutions(){
+    for(int i = 0; i < board.length; i++){
+      for(int j = 0; j < board.length; j++){
+        if(board[i][j] != 0){
+          throw new IllegalStateException("Board must be cleared before finding all solutions");
+        }
+      }
+    }
     return help2(0);
   }
 
@@ -167,9 +211,24 @@ public class QueenBoard{
     System.out.println(one.solve());
     one.clear();
 
-    //tested countSoltuoi
+    //tested countSolutions and the numbers crunched seem right
+    //so by this olgic I can seemingly safely assume solve() works
+    // as intended
+    System.out.println(zero.countSolutions());
     System.out.println(one.countSolutions());
     System.out.println(two.countSolutions());
+    System.out.println(three.countSolutions());
+    System.out.println(four.countSolutions());
+    System.out.println(five.countSolutions());
+    System.out.println(six.countSolutions());
+    System.out.println(seven.countSolutions());
+    System.out.println(eight.countSolutions());
+    System.out.println(nine.countSolutions());
+
+
+    //check to make sure countSolutions keeps the board clear and it does
+    System.out.println(nine);
+
   }
 
 
